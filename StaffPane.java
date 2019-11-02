290367c89720956f7305fb0016c16d2b02d065ba
dhoule <caine2003@gmail.com>
2019-11-02 13:14:37 -0500

    Updates StaffPane.java
    #1 Lots of changes were made to allow the querying of the DB with the
    given credentials.

--- StaffPane.java
+++ StaffPane.java
@@ -1,13 +1,14 @@
 package cs430Project;
 
 import java.util.*;
-import java.util.Vector;
 import java.io.*;
 import java.sql.*;
 
 import java.awt.*; 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.*;         // Using Swing components and containers
 import javax.swing.UIManager; // I don't like how the default UX looks
@@ -26,20 +27,14 @@ import javax.swing.event.ChangeListener;
 import cs430Project.SpringUtilities;
 
 public class StaffPane extends JPanel {
-  /*
-    What is needed:
-    1 JTabbedPane - split in 2
-    Bottom:
-      TextArea that is scrollable
-    Top:
-      TextArea, to write SQL, prevent DROP TABLE
-      Below TextArea:
-        2 buttons: "Clear" & "Submit"
-      Right of TextArea:
-        2 different lists:
-          Top: Tables
-          Botom: Selected Table's attributes
-  */
+  
+
+  JTextArea requestArea, results;
+  String url="jdbc:oracle:thin:@131.230.133.11:1521:cs";
+  private String result = new String();
+  private String queryString = new String();
+  private String usr;
+  private String psw;
 
 
   public StaffPane() {
@@ -52,6 +47,53 @@ public class StaffPane extends JPanel {
     JSplitPane sl = new JSplitPane(SwingConstants.HORIZONTAL, top, bottom);
 
     add(sl, BorderLayout.CENTER);
+    addComponentListener( new ComponentAdapter() {
+      public void componentShown ( ComponentEvent e ){
+        try {
+          DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch(Exception ex) {
+          System.out.println("can't find the driver");
+        }
+        try {
+          // Connection con=DriverManager.getConnection(url,"dhoule","7fPr6p7P");
+          Connection con=DriverManager.getConnection(url,usr,psw);
+          Statement stmt=con.createStatement();
+          ResultSet rs=stmt.executeQuery("SELECT table_name,column_name FROM all_tab_cols where owner='" + usr.toUpperCase() + "'");
+          // TODO subsequent queries: "SELECT table_name,column_name FROM all_tab_cols where owner='" + usr.toUpperCase() +"'"
+          /*Get the meta data of the result set*/
+          ResultSetMetaData rsmd=rs.getMetaData();
+          
+          
+          /*Close the statment and connection to release the resource*/
+          stmt.close();
+          con.close();
+
+        } catch(Exception ex) {
+          System.out.println("SQLException: "+ex);
+        }
+      }
+
+      public void componentHidden( ComponentEvent e ) {
+        resetUserPsw();
+      }
+    });
+  }
+
+  public void setUserName(String newUsr) {
+    if(null != newUsr) {
+      usr = newUsr;
+    }
+  }
+
+  public void setPassword(String newPsw) {
+    if(null != newPsw) {
+      psw = newPsw;
+    }
+  }
+
+  public void resetUserPsw() {
+    usr = null;
+    psw = null;
   }
 
   protected JPanel createTopPanel() {
@@ -60,22 +102,17 @@ public class StaffPane extends JPanel {
 
     Container left = createInputSelection();
 
-    Container right = createTableList();
 
     top.add(left, BorderLayout.LINE_START);
-    top.add(right, BorderLayout.LINE_END);
 
     return top;
   }
 
   protected JPanel createBottomPanel() {
     JPanel bottom = new JPanel();
-    JTextArea results = new JTextArea(10, 20);
-
-    Container right = createAttrList();
+    results = new JTextArea(10, 20);
 
     bottom.add(results, BorderLayout.LINE_START);
-    bottom.add(right, BorderLayout.LINE_END);
   
     return bottom;
   }
@@ -84,7 +121,7 @@ public class StaffPane extends JPanel {
     Container top = new Container();
     top.setLayout(new BoxLayout(top, BoxLayout.PAGE_AXIS));
     
-    JTextArea requestArea = new JTextArea(8, 10);
+    requestArea = new JTextArea(8, 10);
     top.add(requestArea);
 
     Container bottom = new Container();
@@ -94,11 +131,56 @@ public class StaffPane extends JPanel {
     JButton clearBtn = new JButton("Clear");
     clearBtn.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        // TODO clear the TextArea and deselect the lists
+        // clear the TextArea and deselect the lists
         requestArea.setText(null);
       }
     });
     JButton submitBtn = new JButton("Submit");
+    submitBtn.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        queryString = requestArea.getText();
+        System.out.println(queryString);
+        if(queryString.toUpperCase().startsWith("DROP")) {
+          System.out.println("Not allowed to drop table.");
+        } else {
+          try {
+            // Register the oracle driver with DriverManager
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+          } catch(Exception ex) {
+            System.out.println("can't find the driver");
+          }
+          try {
+            results.setText(null);
+            Connection con=DriverManager.getConnection(url,usr,psw);
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery(queryString);
+            ResultSetMetaData rsmd=rs.getMetaData();
+            int numberofcolumn =rsmd.getColumnCount();
+            String columnnames=new String("");
+            // for loop needs to from 1 not 0
+            for(int i = 1; i <= numberofcolumn; i++) {
+               String name=rsmd.getColumnName(i);
+               columnnames=columnnames+"\t"+name;
+            }
+            result+=columnnames;
+            result+="\n";
+            while (rs.next()) {
+            //Read each field of the row, and the for loop also begin with 1
+              for(int i=1;i<=numberofcolumn;i++) {
+                  String s=rs.getString(i);
+                  result+="\t"+s;
+              }
+              result+="\n";
+            }
+            results.setText(result);
+          } catch(Exception ex) {
+            results.setText("SQLException: "+ex);
+            System.err.println("SQLException: "+ex);
+          }
+        }
+
+      }
+    });
 
     bottom.add(clearBtn);
     bottom.add(submitBtn);
@@ -107,40 +189,6 @@ public class StaffPane extends JPanel {
     return top;
   }
 
-  protected Container createTableList(){
-    Container contain = new Container();
-    contain.setMinimumSize(new Dimension(50, 100)); // width, height
-    // JPanel top = new JPanel();
-    JLabel lTop = new JLabel("Available Tables");
-    JList tables = new JList();
-    
-    tables.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-    tables.setLayoutOrientation(JList.VERTICAL_WRAP);
-    contain.add(lTop);
-    contain.add(tables);
 
-    // JPanel bottom = new JPanel();
-    // JLabel lBottom = new JLabel("Available Attributes");
-    // JList attrs = new JList();
-    // attrs.setLayoutOrientation(JList.VERTICAL_WRAP);
-    // bottom.add(lBottom);
-    // bottom.add(attrs);
-
-    return contain;
-  }
-
-  protected Container createAttrList(){
-    Container contain = new Container();
-    contain.setMinimumSize(new Dimension(50, 100)); // width, height
-
-    JLabel lTop = new JLabel("Available Attributes");
-    JLabel lBottom = new JLabel("Available Attributes");
-    JList attrs = new JList();
-    attrs.setLayoutOrientation(JList.VERTICAL_WRAP);
-    contain.add(lBottom);
-    contain.add(attrs);
-
-    return contain;
-  }
 }
 
